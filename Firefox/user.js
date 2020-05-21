af8f88614218a72b80c358b2488c7c82ab824213
David Lemler <dlemler@pm.me>
2020-05-21 01:36:13 -0500

    Add comments to Firefox user.js file

--- Firefox/user.js
+++ Firefox/user.js
@@ -1,11 +1,11 @@
-user_pref("alerts.useSystemBackend", true);
+user_pref("alerts.useSystemBackend", true);//Show website notifications using the operating system notification system.
 user_pref("app.normandy.enabled", true);//Set this to false to disable Firefox surveys.
 user_pref("app.shield.optoutstudies.enabled", false);
-user_pref("app.update.auto", false);
-user_pref("browser.contentblocking.category", "custom");
+user_pref("app.update.auto", false);//Set this to true to enable auto-update.
+user_pref("browser.contentblocking.category", "custom");//Sets Firefox content blocking to custom.
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", true);//Set this to false to disable automatic crash log reporting.
-user_pref("browser.download.useDownloadDir", false);
-user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.download.useDownloadDir", false);//Set this to true to have Firefox remember what download directory to open in based on last download from the website a file is being downloaded from.
+user_pref("browser.tabs.closeWindowWithLastTab", false);//Set this to true to make the browser close the window on close of the last open tab.
 user_pref("browser.taskbar.previews.enable", true);
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"add-ons-button\",\"preferences-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"sync-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"bookmarks-menu-button\",\"personal-bookmarks\",\"canvasblocker_kkapsner_de-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_769cd7ad-ebf8-45c4-b1d6-05f05f0172c3_-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_27db2e44-dbd1-4182-abc8-465a4010d366_-browser-action\",\"_b3e677f4-1150-4387-8629-da738260a48e_-browser-action\",\"_b11bea1f-a888-4332-8d8a-cec2be7d24b9_-browser-action\",\"sidebar-button\"]},\"seen\":[\"bypasspaywalls_bypasspaywalls_weebly_com-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_b3e677f4-1150-4387-8629-da738260a48e_-browser-action\",\"canvasblocker_kkapsner_de-browser-action\",\"addon_darkreader_org-browser-action\",\"notes_mozilla_com-browser-action\",\"plasma-browser-integration_kde_org-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"_b11bea1f-a888-4332-8d8a-cec2be7d24b9_-browser-action\",\"developer-button\",\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_769cd7ad-ebf8-45c4-b1d6-05f05f0172c3_-browser-action\",\"_27db2e44-dbd1-4182-abc8-465a4010d366_-browser-action\",\"chrome-gnome-shell_gnome_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\",\"TabsToolbar\",\"toolbar-menubar\"],\"currentVersion\":16,\"newElementCount\":10}");
 user_pref("browser.uidensity" 1);
