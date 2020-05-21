b2493c482b74750e658c6286e75f9a24d529ead7
David Lemler <dlemler@pm.me>
2020-05-20 20:12:57 -0500

    Add Firefox configurations

---
+++ Firefox/user.js
@@ -0,0 +1,39 @@
+user_pref("alerts.useSystemBackend", true);
+user_pref("app.normandy.enabled", true);//Set this to false to disable Firefox surveys.
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.update.auto", false);
+user_pref("browser.contentblocking.category", "custom");
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", true);//Set this to false to disable automatic crash log reporting.
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.taskbar.previews.enable", true);
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"add-ons-button\",\"preferences-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"sync-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"bookmarks-menu-button\",\"personal-bookmarks\",\"canvasblocker_kkapsner_de-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_769cd7ad-ebf8-45c4-b1d6-05f05f0172c3_-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_27db2e44-dbd1-4182-abc8-465a4010d366_-browser-action\",\"_b3e677f4-1150-4387-8629-da738260a48e_-browser-action\",\"_b11bea1f-a888-4332-8d8a-cec2be7d24b9_-browser-action\",\"sidebar-button\"]},\"seen\":[\"bypasspaywalls_bypasspaywalls_weebly_com-browser-action\",\"redirector_einaregilsson_com-browser-action\",\"_b3e677f4-1150-4387-8629-da738260a48e_-browser-action\",\"canvasblocker_kkapsner_de-browser-action\",\"addon_darkreader_org-browser-action\",\"notes_mozilla_com-browser-action\",\"plasma-browser-integration_kde_org-browser-action\",\"treestyletab_piro_sakura_ne_jp-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"_b11bea1f-a888-4332-8d8a-cec2be7d24b9_-browser-action\",\"developer-button\",\"_a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7_-browser-action\",\"_769cd7ad-ebf8-45c4-b1d6-05f05f0172c3_-browser-action\",\"_27db2e44-dbd1-4182-abc8-465a4010d366_-browser-action\",\"chrome-gnome-shell_gnome_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\",\"TabsToolbar\",\"toolbar-menubar\"],\"currentVersion\":16,\"newElementCount\":10}");
+user_pref("browser.uidensity" 1);
+user_pref("datareporting.healthreport.uploadEnabled", true);//Set this to false to disable telemetry.
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("dom.event.contextmenu.enabled", true);
+user_pref("dom.forms.selectSearch", true);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("general.useragent.override", "Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/76.0");
+user_pref("general.warnOnAboutConfig", false);
+user_pref("media.autoplay.default", 1);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.http.sendRefererHeader", 2);
+user_pref("network.IDN_show_punycode", true);
+user_pref("print.print_footerleft", "");
+user_pref("print.print_footerright", "");
+user_pref("print.print_headerleft", "");
+user_pref("print.print_headerright", "");
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.firstparty.isolate", false);
+user_pref("privacy.trackingprotection.cryptomining.enabled", false);
+user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
+user_pref("privacy.trackingprotection.pbmode.enabled", false);
+user_pref("reader.color_scheme", "dark");
+user_pref("reader.content_width", 9);
+user_pref("reader.font_size", 7);
+user_pref("reader.line_height", 3);
+user_pref("security.identityblock.show_extended_validation", true);
+user_pref("security.secure_connection_icon_color_gray", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
